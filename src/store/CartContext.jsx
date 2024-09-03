d60ca2f3b8425000629d5f48878abab191a365e7
ragul2808 <ragulraja28@gmail.com>
2024-09-03 16:22:18 +0530

    Implement cartReducer to handle ADD_ITEM action: Describes the key change made in the reducer.

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -7,15 +7,35 @@ const CartContext = createContext({
 })
 function cartReducer(state, action){
     if(action.type === 'ADD_ITEM') {
-        //
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id ===action.item.id
+        );
+        const updatedItems = [...state.items];
+
+        if (existingCartItemIndex > -1) {
+            const existingItem = state.items[existingCartItemIndex]
+            const updatedItems = {
+                ...existingItem,
+                quantity: existingCartItemIndex.quantity + 1
+            };
+            updatedItems[existingCartItemIndex] = updatedItems;
+
+        }else {
+            updatedItems.push({...action.item, quantity: 1})
+
+        }
+
+        return{...state, items: updatedItems};
     }
+
     if(action.type === 'REMOVE_ITEM') {
         //
     }
+    return state;
 }
 
 export function CartContextProvider({children}) {
-    useReducer(cartReducer);
+    useReducer(cartReducer, { items: [] });
 
     return (
         <CartContext.Provider>{children}</CartContext.Provider>
